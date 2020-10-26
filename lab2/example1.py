daa5884ac40cb95bf5fdba444fee60209d9a862d
y14562434 <serficelis@gmail.com>
2020-10-26 08:24:18 +0000

    abulute value

--- lab2/example1.py
+++
@@ -1,4 +0,0 @@
-a = float(input("enter a number"))
-b = float(input("enter number"))
-c= ((a**2)+(b**2))**0.5
-print(c)
---
+++ lab3/example1.py
@@ -0,0 +1,7 @@
+num = int(input("write a number"))
+
+if num<0:
+  print(-num)
+else :
+  print(num)
+
