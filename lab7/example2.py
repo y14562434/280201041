e9ad21626d9029d079dd254eff9c4a43c81c0309
y14562434 <serficelis@gmail.com>
2020-11-30 09:11:47 +0000

    books dict

---
+++ lab7/example2.py
@@ -0,0 +1,10 @@
+books = ["ULYSSES","ANIMAL FARM","BRAVE NEW WORLD","ENDER'S GAME"]
+book_dict = {}
+
+for book in books:
+  charact = len(book)
+  unique_charact = len(set(book))
+  value = (charact, unique_charact)
+  book_dict[book] = value
+print(book_dict)  
+
